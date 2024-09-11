db4cfd9e42b39814f19a0041233925d7def8c189
Felipe Sepulveda <felipe.sepulveda@dnb.no>
2024-09-11 08:46:27 +0200

    added speed insight, should work

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,26 @@ import MatchGame from './MatchGame';
 import TranslationGame from './TranslationGame';
 import './App.css';
 import { getWeekNumber } from './utils/dates';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
 const App: React.FC = () => {
   const [currentDate] = useState(new Date());
 
   const week = getWeekNumber(currentDate);
 
+  injectSpeedInsights();
+
   return (
     <Router>
       <div className="app-container">
         <h1 className="main-title">Language Learning Games - Week {week}</h1>
+  
         <nav className="menu">
           <Link to="/match-game" className="menu-item">Word Matching Game</Link>
           <Link to="/translation-game" className="menu-item">Translation Game</Link>
         </nav>
 
         <div className="content">
-
           <Routes>
             <Route path="/match-game" element={<MatchGame />} />
             <Route path="/translation-game" element={<TranslationGame />} />

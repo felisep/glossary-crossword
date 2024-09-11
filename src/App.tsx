985db94e4c55f01dbfcc25cfe1a5a2ca70e71c81
Felipe Sepulveda <felipe.sepulveda@dnb.no>
2024-09-11 08:13:24 +0200

    set week number

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import MatchGame from './MatchGame';
 import TranslationGame from './TranslationGame';
 import './App.css';
+import { getWeekNumber } from './utils/dates';
 
 const App: React.FC = () => {
+  const [currentDate] = useState(new Date());
+
+  const week = getWeekNumber(currentDate);
+
   return (
     <Router>
       <div className="app-container">
-        <h1 className="main-title">Language Learning Games - Week 37</h1>
+        <h1 className="main-title">Language Learning Games - Week {week}</h1>
         <nav className="menu">
           <Link to="/match-game" className="menu-item">Word Matching Game</Link>
           <Link to="/translation-game" className="menu-item">Translation Game</Link>
         </nav>
 
         <div className="content">
+
           <Routes>
             <Route path="/match-game" element={<MatchGame />} />
             <Route path="/translation-game" element={<TranslationGame />} />
